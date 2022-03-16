9f0c94c15714aec4b0251c433c2593880a6d50f1
Christina Tolcidas <mtolcidas@frontlineed.com>
2022-03-16 11:00:35 +0800

    FLPG-16995: Pagination Enhancement

--- 4ward/ui/button/button.spec.tsx
+++ 4ward/ui/button/button.spec.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import {
   render, cleanup, fireEvent, screen,
 } from '@testing-library/react';
-import { PrimaryButton } from './button.composition';
+import {
+  PrimaryButton, DangerButton, StandardButton, DisabledButton, SuccessButton,
+} from './button.composition';
 import { Button } from './button';
 
 describe('initial test for loader component', () => {
@@ -16,11 +18,19 @@ describe('initial test for loader component', () => {
   });
 
   it('should render correctly', () => {
-    const basic = render(<PrimaryButton />);
-    expect(basic).toBeTruthy();
+    const standard = render(<StandardButton />);
+    const primary = render(<PrimaryButton />);
+    const danger = render(<DangerButton />);
+    const sucess = render(<SuccessButton />);
+    const disabled = render(<DisabledButton />);
+    expect(primary).toBeTruthy();
+    expect(danger).toBeTruthy();
+    expect(standard).toBeTruthy();
+    expect(sucess).toBeTruthy();
+    expect(disabled).toBeTruthy();
   });
   it('should click successfully', () => {
-    render(<Button buttonType="danger" />);
+    render(<DangerButton />);
     fireEvent.click(screen.getByTestId('button'));
     expect(screen).toBeTruthy();
   });
